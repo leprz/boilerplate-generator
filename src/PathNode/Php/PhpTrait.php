b96593a618cfd71a57e14912adcf1cf14b94e363
Przemek Łęczycki <przemek281@gmail.com>
2021-03-07 15:38:19 +0100

    feat: add traits support

---
+++ src/PathNode/Php/PhpTrait.php
@@ -0,0 +1,20 @@
+<?php
+
+/*
+ *
+ * This file is part of leprz/boilerplate-generator
+ *
+ * Copyright (c) 2021. Przemek Łęczycki <leczycki.przemyslaw@gmail.com>
+ */
+
+declare(strict_types=1);
+
+namespace Leprz\Boilerplate\PathNode\Php;
+
+/**
+ * @package Leprz\Boilerplate\PathNode\Php
+ */
+class PhpTrait extends PhpClass
+{
+
+}
